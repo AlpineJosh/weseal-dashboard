f8e03537a906e214cf4f32f79b5d7cfbf09b830f
Josh Hobson <118217725+AlpineJosh@users.noreply.github.com>
2024-07-15 17:07:03 +0100

    Testing auth

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,21 +1,55 @@
 "use client";
 
+import { supabase } from "~/supabase/client";
 import { api } from "~/trpc/react";
 
 export const runtime = "edge";
 
 export default function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
-  const { data } = api.component.all.useQuery();
+  const { data } = api.component.all.useQuery({
+    pagination: {
+      page: 4,
+      size: 20,
+    },
+    sort: [
+      {
+        field: "id",
+        order: "asc",
+      },
+    ],
+  });
+
+  const signInWithSSO = async () => {
+    await supabase.auth.signInWithSSO({
+      domain: "weseal.com",
+    });
+  };
+
+
   console.log(data)
   return (
       <main className="container h-screen py-16">
         <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            {data?.map((component) => (
-              <div key={component.id}>{component.id}</div>
-            ))}
-          </h1>
+          <button onClick={signInWithSSO}>Login</button>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Description</th>
+                <th>Category</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((component) => (
+                <tr key={component.id}>
+                  <td>{component.id}</td>
+                  <td>{component.description}</td>
+                  <td>{component.category}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </main>
   );
