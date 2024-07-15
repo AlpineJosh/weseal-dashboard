09cb1f9b86be380ecd7369f49e7d1d9f7511c24c
Josh Hobson <118217725+AlpineJosh@users.noreply.github.com>
2024-07-15 17:53:11 +0100

    Fixing Env issus

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-properties */
+/* eslint-disable turbo/no-undeclared-env-vars */
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
@@ -8,30 +10,22 @@ export const env = createEnv({
       .enum(["development", "production", "test"])
       .default("development"),
   },
-  /**
-   * Specify your server-side environment variables schema here.
-   * This way you can ensure the app isn't built with invalid env vars.
-   */
+
   server: {
     DATABASE_URL: z.string().url(),
   },
 
-  /**
-   * Specify your client-side environment variables schema here.
-   * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
-   */
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
+
   /**
-   * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
+   * Destructure all client side variables from `process.env` to make sure they aren't tree-shaken away.
    */
   experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-  },
-  skipValidation:
-    !!process.env.CI || process.env.npm_lifecycle_event === "lint",
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  }
 });
